60543c19d0d898fc51c8fa36777ac160a44c7d60
Daemon <paakwesinunoo135@gmail.com>
2023-01-17 12:41:35 -0800

    furthur changes

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,9 @@
-import {HiOutlineDesktopComputer} from 'react-icons/hi'
-import {BsCodeSlash} from 'react-icons/bs'
-import {ImAndroid} from 'react-icons/im'
-import {AiFillCamera} from 'react-icons/ai'
-import {TbBrandLoom} from 'react-icons/tb'
-import {GoGlobe} from 'react-icons/go'
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { BsCodeSlash } from "react-icons/bs";
+import { ImAndroid } from "react-icons/im";
+import { AiFillCamera } from "react-icons/ai";
+import { TbBrandLoom } from "react-icons/tb";
+import { GoGlobe } from "react-icons/go";
 const Section = () => {
   return (
     <>
@@ -20,7 +20,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c2fc413566a09e01f711c4_work-9-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Graphic Design</div>
@@ -31,7 +31,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009bf69ff41454c6fe63_work-2-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Print Design</div>
@@ -41,7 +41,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009b617cac9b64365383_work-1-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Mobile Apps</div>
@@ -51,7 +51,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009b52f6a18001ef67c2_work-3-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">3D Drawing</div>
@@ -64,7 +64,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009b52f6a18001ef67c3_work-5-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Interior Design</div>
@@ -74,7 +74,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009b617cac9b64365384_work-4-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Web Design</div>
@@ -84,7 +84,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009c739f273554ad015b_work-7-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Graphic Design</div>
@@ -94,7 +94,7 @@ const Section = () => {
             <img
               src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3009c617cac9b64365385_work-8-p-500x334.jpeg"
               alt="Avatar"
-              class="image"
+              class="staff-image"
             />
             <div class="overlay">
               <div class="text">Home Design</div>
@@ -106,73 +106,152 @@ const Section = () => {
           <div className="contact-btn">Contact Now</div>
         </div>
       </div>
-<hr />
-      <div className="sec-head">
-        <h1 className="sec-header">Services</h1>
-        <h4  className='sec-para'>Scope of our featured premium services</h4>
- <br />
- <br/>
+      <hr />
 
-        <div className="services">
-    <div className="tip">
-    <div className="serv-icon"><HiOutlineDesktopComputer size={50}/></div>
+      <h1 className="sec-header">Services</h1>
+      <h4 className="sec-para">Scope of our featured premium services</h4>
+      <br />
+      <br />
+
+      <div className="services">
+        <div className="tip">
+          <div className="serv-icon">
+            <HiOutlineDesktopComputer size={50} />
+          </div>
           <div className="serv-title">GRAPHIC DESIGN</div>
           <br />
           <div className="serv-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique. Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim eros elementum tristique. Duis cursus, viverra ornare,
+            eros dolor interdum nulla, commodo diam libero vitae
           </div>
-    </div>
-
-
-     <div className="tip">
-     <div className="serv-icon"><BsCodeSlash size={50}/></div>
-     <br />
-          <div className="serv-title">WEB DESIGN</div>
-          <div className="serv-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique. Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae
-          </div>
-     </div>
-
-    <div className="tip">
-    <div className="serv-icon"><ImAndroid size={50}/></div>
-    <br />
-          <div className="serv-title">APP DESIGN</div>
-          <div className="serv-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique. Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae
-          </div>
-    </div>
-
- 
         </div>
-        <br />
 
-        <div className="services">
-      <div className="tip">
-      <div className="serv-icon"><AiFillCamera size={50}/></div>
+        <div className="tip">
+          <div className="serv-icon">
+            <BsCodeSlash size={50} />
+          </div>
+          <div className="serv-title">WEB DESIGN</div>
+          <br />
+          <div className="serv-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim eros elementum tristique. Duis cursus, viverra ornare,
+            eros dolor interdum nulla, commodo diam libero vitae
+          </div>
+        </div>
+
+        <div className="tip">
+          <div className="serv-icon">
+            <ImAndroid size={50} />
+          </div>
+          <div className="serv-title">APP DESIGN</div>
+          <br />
+          <div className="serv-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim eros elementum tristique. Duis cursus, viverra ornare,
+            eros dolor interdum nulla, commodo diam libero vitae
+          </div>
+        </div>
+      </div>
+      <br />
+
+      <div className="services">
+        <div className="tip">
+          <div className="serv-icon">
+            <AiFillCamera size={50} />
+          </div>
           <div className="serv-title">PHOTOGRAPTHY</div>
           <br />
           <div className="serv-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique. Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim eros elementum tristique. Duis cursus, viverra ornare,
+            eros dolor interdum nulla, commodo diam libero vitae
           </div>
-      </div>
+        </div>
 
-     <div className="tip">
-     <div className="serv-icon"><TbBrandLoom size={50}/></div>
+        <div className="tip">
+          <div className="serv-icon">
+            <TbBrandLoom size={50} />
+          </div>
           <div className="serv-title">BRANDING</div>
           <br />
           <div className="serv-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique. Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim eros elementum tristique. Duis cursus, viverra ornare,
+            eros dolor interdum nulla, commodo diam libero vitae
           </div>
-     </div>
-          
-     <div className="tip">
-     <div className="serv-icon"><GoGlobe size={50}/></div>
+        </div>
+
+        <div className="tip">
+          <div className="serv-icon">
+            <GoGlobe size={50} />
+          </div>
           <div className="serv-title">HOSTING</div>
           <br />
           <div className="serv-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique. Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim eros elementum tristique. Duis cursus, viverra ornare,
+            eros dolor interdum nulla, commodo diam libero vitae
           </div>
-     </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <h1 className="sec-header">AWESOME TEAM</h1>
+      <p className="sec-para">
+        Setibulum rutrum quam vitae fringilla tincidunt
+      </p>
+
+      <div className="staffs">
+        <div className="staff">
+          <div className="staff-image">
+            <img
+              src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3174a617cac9b643685b5_team-1.jpg"
+              alt="staff"
+              height='290'
+              width='250'
+            />
+          </div>
+          <div className="staff-name">Isaiah Psalm</div>
+          <div className="position">Creative Director</div>
+        </div>
+        <div className="staff">
+          <div className="staff-image">
+            <img
+              src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3174a739f273554ad25b4_team-2.jpg"
+              alt=""
+              height='290'
+              width='250'
+            />
+          </div>
+          <div className="staff-name">Jeniah Ortega</div>
+          <div className="position">Graphic Designer</div>
+        </div>
+
+        <div className="staff">
+          <div className="staff-image">
+            <img
+              src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3174b739f273554ad25b5_team-3.jpg"
+              alt=""
+              height='290'
+              width='250'
+            />
+          </div>
+          <div className="staff-name">Eugene Davis</div>
+          <div className="position">Web Developer</div>
+        </div>
+
+        <div className="staff">
+          <div className="staff-image">
+            <img
+              src="https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c3174b52f6a18001ef8d42_team-4.jpg"
+              alt=""
+              height='290'
+              width='250'
+            />
+          </div>
+          <div className="staff-name">Wayne Moore</div>
+          <div className="position">Web Designer</div>
         </div>
       </div>
     </>
